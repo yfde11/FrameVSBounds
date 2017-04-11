dcd472cd365bc9c8905bb27098a4cbbc98e8e281
MichaelXiao <y19938222@gmail.com>
2017-04-11 11:38:22 +0800

    add code for frame

--- FramesVSBounds/FramesVSBounds/ViewController.swift
+++ FramesVSBounds/FramesVSBounds/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueView: UIView!
+    let customView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("BlueViewframe:\t X:\(blueView.frame.origin.x)\t Y:\(blueView.frame.origin.y)\t Width: \(blueView.frame.size.width)\t Height:\(blueView.frame.size.height)")
+        
+        setUpCustomView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setUpCustomView() {
+        blueView.addSubview(customView)
+        customView.backgroundColor = UIColor.brown
+        print("customViewframe:\t X:\(customView.frame.origin.x)\t Y:\(customView.frame.origin.y)\t Width: \(customView.frame.size.width)\t Height:\(customView.frame.size.height)")
+        blueView.bounds.origin = CGPoint(x: -10, y: -10)
+        print("blueViewbounds:\t X:\(blueView.bounds.origin.x)\t Y:\(blueView.bounds.origin.y)\t Width:\(blueView.bounds.size.width)\t Height:\(blueView.bounds.size.height)")
     }
-
-
+    
 }
 
